528c02f22ac181cb6935d3e7000a6025aa08a70d
Perkles <fortesvitoria@gmail.com>
2023-11-29 20:20:59 -0300

    troca index.html por index.php

--- conexao.php
+++ conexao.php
@@ -3,10 +3,10 @@
     $banco = mysqli_connect("localhost","root","Vi07MySQL@","vitoschool");
 
 //testa se conexão funcionou
-    if(!$banco) {
-        echo "NÃO CONECTOU NO BANCO DE DADOS :( <br>".mysqli_connect_error();
-    } else {
-        echo "CONECTOU :) <br>";
-    }
+    // if(!$banco) {
+    //     echo "NÃO CONECTOU NO BANCO DE DADOS :( <br>".mysqli_connect_error();
+    // } else {
+    //     echo "CONECTOU :) <br>";
+    // }
 
 ?>
