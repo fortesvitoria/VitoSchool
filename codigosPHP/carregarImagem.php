cd999d59a2db7be4c6b5e3a990f75a1f3df51bff
Perkles <fortesvitoria@gmail.com>
2023-12-06 20:55:37 -0300

    cria codigo para carregar imagem blob

---
+++ codigosPHP/carregarImagem.php
@@ -0,0 +1,22 @@
+<?php
+// Inclui o arquivo de conexão com o banco de dados
+include("conexao.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $cpf = $_POST["cpf"];
+
+    $query = "SELECT foto FROM cadastro WHERE cpf = $cpf";
+    $resultado = mysqli_query($banco, $query);
+
+    if ($resultado && mysqli_num_rows($resultado) > 0) {
+        $row = mysqli_fetch_assoc($resultado);
+        $imagem_binaria = $row['foto'];
+
+        // Define os headers para indicar que é uma imagem
+        header("Content-type: image/jpeg"); // Substitua por mime type adequado se necessário
+        echo $imagem_binaria; // Exibe a imagem diretamente na tela
+    } else {
+        echo "Imagem não encontrada.";
+    }
+}
+?>
